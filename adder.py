487622faa532f67a7f1bbea04677c95c2c3bb5d6
natchin76 <57409149+natchin76@users.noreply.github.com>
2019-11-06 00:59:06 +0530

    adder2
    Arbitrary number of inputs are allowed

--- adder.py
+++ adder.py
@@ -1,9 +1,14 @@
 """
 Created on Wed Nov  6 00:23:37 2019
 Function file that adds two numbers/strings/lists...
+Arbitrary number of inputs are allowed
 @author: CHINTAN
 """
-def add(a,b):
-    return(a+b)
+def add(*s):
+    a=0
+    for i in range(len(s)):
+        a=a+s[i]
+    return(a)        
+        
     
-print(add(1,3))
+print(add(1,3,4))
